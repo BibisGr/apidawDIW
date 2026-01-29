a0681a4ea1c95041c07e08c851251ed6fc4fe74e
Bibis Gr <bertha.garrastacho@digitechfp.com>
2026-01-29 12:23:43 +0100

    base

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+
+function App() {
+
+  return (
+      <>
+          <h1 className="display-1">Hola desde Api App React</h1>
+      </>
+  )
+}
+
+export default App
